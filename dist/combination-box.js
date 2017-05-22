6d053cb985a9ed1c1b1a6c8fca2f59de9e25c3ee
Jacob Roman <jacob.roman@deque.com>
2017-05-22 10:02:10 -0400

    Pulled in new stuff

--- dist/combination-box.js
+++ dist/combination-box.js
@@ -3497,6 +3497,12 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
       process.removeListener = noop;
       process.removeAllListeners = noop;
       process.emit = noop;
+      process.prependListener = noop;
+      process.prependOnceListener = noop;
+
+      process.listeners = function (name) {
+        return [];
+      };
 
       process.binding = function (name) {
         throw new Error('process.binding is not supported');
